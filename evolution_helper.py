2e6a9ca5d9e71a7c61218deb1fbcb7681330299f
James Crompton <jamescrompton18@gmail.com>
2018-01-31 21:57:56 -0500

    Evolve

---
+++ evolution_helper.py
@@ -0,0 +1,112 @@
+import math
+
+import numpy as np
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization, LeakyReLU, PReLU
+from keras import backend as K
+from keras.callbacks import TerminateOnNaN, EarlyStopping
+
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import normalize, scale, minmax_scale
+from bitstring import BitArray
+
+def build_base_nn_eval_func(x_train, y_train, x_test, y_test, max_num_layers=None,
+                            max_num_neurons=None, verbosity=0, epochs=1, *args, **kwargs):
+    max_num_layers = max_num_layers if max_num_neurons else 4
+    max_num_neurons = max_num_neurons if max_num_neurons else 1024
+    dropouts = kwargs.get('dropouts')
+    activations = kwargs.get('activations')
+    optimizers = kwargs.get('optimizers')
+    metrics = kwargs.get('metrics')
+    scalers = kwargs.get('scalers')
+    verbosity = kwargs.get('verbosity')
+    metric = kwargs.get('metric')
+    shape = x_train.shape[1]
+    predictors = y_train.shape[1]
+    epochs = epochs
+
+    def base_eval_func(sequence, ret=False, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, verbose=verbosity, metric=metric, epochs=epochs, gene_len=False):
+        if gene_len:
+            return transcribe_base(sequence, max_num_layers, max_num_neurons, activations=activations, dropouts=dropouts, optimizers=optimizers, scalers=scalers, gene_len=True)
+        genome, hidden_architecture, dropout, optimizer, activation, batch_norm, batch_size, scaler = transcribe_base(sequence, max_num_layers, max_num_neurons, activations=activations, dropouts=dropouts, optimizers=optimizers, scalers=scalers)
+        advanced_activation = True if type(activation) != str else False
+        metric = metric if metric else mean_squared_error
+        model = Sequential()
+        model.add(Dropout(dropout, input_shape=(shape,)))
+        for layer_params in hidden_architecture:
+            layer_size = layer_params[0]
+            try:
+                hidden_dropout = layer_params[1]
+            except IndexError:
+                hidden_dropout = dropout
+            if advanced_activation:
+                model.add(Dense(layer_size))
+                model.add(activation)
+            else:
+                model.add(Dense(layer_size, activation=activation))
+            if batch_norm:
+                model.add(BatchNormalization())
+            model.add(Dropout(hidden_dropout))
+        model.add(Dense(predictors))
+        model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=metrics)
+
+        if scaler:
+            x_train = scaler(x_train)
+            x_test = scaler(x_test)
+
+        model.fit(x_train, y_train, epochs=epochs, verbose=verbose, callbacks=[TerminateOnNaN()])
+        if ret:
+            return model
+        pred = model.predict(x_test)
+
+        try:
+            score = metric(y_test, pred)
+        except ValueError:
+            score = 999999999
+        return score,
+    return base_eval_func
+
+def transcribe_base(sequence, max_num_layers, max_num_neurons, min_batch=16, max_batch=128, activations=None, dropouts=None, optimizers=None, scalers=None, gene_len=False, *args, **kwargs):
+    activations = activations if activations else ['relu', 'tanh', 'sigmoid', 'elu', 'linear', LeakyReLU(), 'selu', 'hard_sigmoid']
+    optimizers = optimizers if optimizers else ['RMSprop', 'adam', 'nadam', 'SGD']
+    dropouts = dropouts if dropouts else [0.0, 0.01, 0.1, 0.25, 0.333, 0.5, 0.6, 0.75]
+    scalers = scalers if scalers else [None, normalize, minmax_scale, scale]
+    len_of_layer_gene = int(math.log(max_num_layers, 2))
+    len_of_hidden_arch_gene = sum([int(math.log(max_num_neurons,
+     2)) - i for i in range(max_num_layers)])
+    len_of_dropout_gene = int(math.log(len(dropouts), 2))
+    len_of_optimizer_gene = int(math.log(len(optimizers), 2))
+    len_of_activation_gene = int(math.log(len(activations), 2))
+    len_of_batch_norm_gene = 1
+    len_of_batch_size = int(math.log(max_batch, 2) - math.log(min_batch, 2))
+    len_of_scalers = int(math.log(len(scalers), 2))
+    genome = [len_of_layer_gene, len_of_hidden_arch_gene,
+                      len_of_dropout_gene, len_of_optimizer_gene,
+                      len_of_activation_gene, len_of_batch_norm_gene,
+                      len_of_batch_size, len_of_scalers]
+    if gene_len:
+        return sum(genome)
+    assert len(sequence) == sum(genome), "Length of input sequence must match length of desired individual:{}, given length {}.".format(sum(genome), len(sequence))
+
+    num_layers = transcribe(sequence, len_of_layer_gene)+1
+    # Generate hidden architecture
+    hidden_architecture = []
+    idx = max_num_layers
+    window = int(math.log(max_num_neurons, 2))
+    for _ in range(num_layers):
+        num_of_neurons = BitArray(sequence[idx:idx+window]).uint
+        hidden_architecture.append((num_of_neurons, ))
+        idx += window
+        window -= 1
+    dropout = dropouts[transcribe(sequence, sum(genome[:2])+genome[2], start=sum(genome[:2]))]
+    optimizer = optimizers[transcribe(sequence, sum(genome[:3])+genome[3], start=sum(genome[:3]))]
+    activation = activations[transcribe(sequence, sum(genome[:4])+genome[4], start=sum(genome[:4]))]
+    batch_norm = [True, False][transcribe(sequence, sum(genome[:5])+genome[5], start=sum(genome[:5]))]
+    batch_size = (transcribe(sequence, sum(genome[:6])+genome[6], start=sum(genome[:6]))+1)*min_batch
+    scaler = scalers[transcribe(sequence, sum(genome[:7])+genome[7], start=sum(genome[:7]))]
+    return genome, hidden_architecture, dropout, optimizer, activation, batch_norm, batch_size, scaler
+
+def transcribe(sequence, end, start=0):
+    return BitArray(sequence[start:end]).uint
---
+++ evolve.py
@@ -0,0 +1,126 @@
+import os
+import argparse
+import time
+import names
+
+import numpy as np
+import pandas as pd
+
+from scipy.stats import bernoulli
+from bitstring import BitArray
+
+from deap import base, creator, tools, algorithms
+
+from evolution_helper import build_base_nn_eval_func
+
+np.random.seed(42)
+
+def get_data(data_path):
+    x_train = pd.read_csv('{}/x_train.csv'.format(data_path)).values
+    y_train = pd.read_csv('{}/x_train.csv'.format(data_path)).values
+    x_test = pd.read_csv('{}/x_train.csv'.format(data_path)).values
+    y_test = pd.read_csv('{}/x_train.csv'.format(data_path)).values
+    return x_train, y_train, x_test, y_test
+
+
+def save_model(model, path, model_name):
+    model_path = '{}/{}'.format(path, model_name)
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+    model_json = model.to_json()
+    with open('{}/frozen_params.json'.format(model_path), 'w') as json_file:
+        json_file.write(model_json)
+    model.save_weights('{}/frozen_weights.h5'.format(model_path))
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_layers', action='store', default=4)
+    parser.add_argument('--max_neurons', action='store', default=1024)
+    parser.add_argument('--population_size', action='store', default=15)
+    parser.add_argument('--data_path', action='store')
+    parser.add_argument('--generations', action='store', default=25)
+    parser.add_argument('--gene_mutation', action='store', default=0.2)
+    parser.add_argument('--gene_crossover', action='store', default=0.4)
+    parser.add_argument('--verbosity', action='store', default=1)
+    parser.add_argument('--epochs', action='store', default=1)
+    parser.add_argument('--save_k_best', action='store', default=1)
+    parser.add_argument('--build_epochs', action='store', default=10)
+    parser.add_argument('--frozen_models_path', action='store', default=None)
+    return parser
+
+def build_base_toolbox(pop_size, eval_func):
+    creator.create('FitnessMax', base.Fitness, weights = (-1.0,))
+    creator.create('Individual', list , fitness = creator.FitnessMax)
+
+    toolbox = base.Toolbox()
+    toolbox.register('binary', bernoulli.rvs, 0.5)
+    toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.binary,
+    n = gene_length)
+    toolbox.register('population', tools.initRepeat, list , toolbox.individual)
+
+    toolbox.register('mate', tools.cxOrdered)
+    toolbox.register('mutate', tools.mutShuffleIndexes, indpb = 0.6)
+    toolbox.register('select', tools.selRoulette)
+    toolbox.register('evaluate', eval_func)
+
+    population = toolbox.population(n = pop_size)
+
+    return creator, toolbox, population
+
+if __name__ == '__main__':
+    start_time = time.time()
+    parser = build_parser()
+    arguments = parser.parse_args()
+    # Gather variables
+    data_path = str(arguments.data_path)
+    max_layers = int(arguments.max_layers)
+    max_neurons = int(arguments.max_neurons)
+    pop_size = int(arguments.population_size)
+    generations = int(arguments.generations)
+    gene_mut = float(arguments.gene_mutation)
+    gene_crsv = float(arguments.gene_crossover)
+    verbosity = int(arguments.verbosity)
+    epochs = int(arguments.epochs)
+    k_to_save = int(arguments.save_k_best)
+    build_epochs = int(arguments.build_epochs)
+    frozen_models_path = arguments.frozen_models_path if arguments.frozen_models_path else data_path
+
+    # Check arguments are clean
+    assert os.path.exists(data_path), 'Make sure --data_path argument points to an existing directory. Given directory {}.'.format(data_path)
+    assert gene_mut <= 1.0, 'Gene mutation cannot be greater than 1...'
+    assert gene_crsv <= 1.0, 'Gene crossover cannot be greater than 1...'
+    assert os.path.exists(frozen_models_path), 'Make sure frozen_models_path points to an existing directory. Given path {}'.format(frozen_models_path)
+    # Get data
+    x_train, y_train, x_test, y_test = get_data(data_path)
+    # Build toolbox and model function
+    eval_func = build_base_nn_eval_func(x_train, y_train, x_test, y_test, max_num_layers=max_layers, max_num_neurons=max_neurons, verbosity=verbosity, epochs=epochs)
+    gene_length = eval_func([1], gene_len=True)
+
+    creator.create('FitnessMax', base.Fitness, weights = (-1.0,))
+    creator.create('Individual', list , fitness = creator.FitnessMax)
+
+    toolbox = base.Toolbox()
+    toolbox.register('binary', bernoulli.rvs, 0.5)
+    toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.binary,
+    n = gene_length)
+    toolbox.register('population', tools.initRepeat, list , toolbox.individual)
+
+    toolbox.register('mate', tools.cxOrdered)
+    toolbox.register('mutate', tools.mutShuffleIndexes, indpb = 0.6)
+    toolbox.register('select', tools.selRoulette)
+    toolbox.register('evaluate', eval_func)
+
+    population = toolbox.population(n=pop_size)
+
+    algorithms.eaSimple(population, toolbox,cxpb=gene_crsv, mutpb=gene_mut,
+    ngen=generations, verbose=False if verbosity < 1 else True)
+
+    end_time = round((time.time() - start_time)/60.0, 3)
+    best_individuals = tools.selBest(population, k=k_to_save)
+    best_evolutionary_score = eval_func(best_individuals[0])
+    print('Evolution complete, best score {} in {} minutes{}\n'.format(best_evolutionary_score, end_time, '!' if best_evolutionary_score < 1.0 else '...'))
+    print('Now saving top {} models to {}...'.format(k_to_save, frozen_models_path))
+    for dna, name in zip(best_individuals, [names.get_full_name() for _ in range(k_to_save)]):
+        model = eval_func(dna, ret=True, epochs=build_epochs)
+        save_model(model, frozen_models_path, name)
